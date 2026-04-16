3ffaf24cbd7b4b2519d0ebeae310f4e053b80658
Corey B <14075562+coreyb-git@users.noreply.github.com>
2026-04-17 00:08:30 +1000

    Cleaned up the filetype-colours code

--- lua/plugins/ui/my-filetype-colours.lua
+++ lua/plugins/ui/my-filetype-colours.lua
@@ -1,5 +1,8 @@
 local leader_toggle = Leader_ui .. "F"
 
+local buffers_text = { "asciidoc", "markdown", "text", "vimwiki" }
+local buffers_code = { "cs", "json", "lua", "php", "ruby", "sh" }
+
 local enabled = true
 
 local function toggle()
@@ -14,9 +17,6 @@ local function toggle()
 end
 
 vim.keymap.set("n", leader_toggle, toggle, { desc = "Toggle [F]iletype Colours" })
-
-local buffers_text = { "asciidoc", "markdown", "text", "vimwiki" }
-local buffers_code = { "cs", "json", "lua", "php", "ruby", "sh" }
 
 -- qf are quickfix windows
 -- trouble is the "trouble" plugin for finding fixme's and todo's etc.
@@ -38,80 +38,80 @@ local scheme_default = {
 
 local scheme_text = {
 	colo = "melange",
+	bg = "light",
 	extras_func = default_extras,
 }
 
 local scheme_code = scheme_default
 
+local function set_colours(scheme)
+	local tbl = vim.tbl_deep_extend("force", scheme_default, scheme)
+
+	if tbl.colo ~= nil then
+		vim.cmd("colo " .. tbl.colo)
+	end
+	if tbl.bg ~= nil then
+		vim.cmd("set bg=" .. tbl.bg)
+	end
+	if tbl.extras_func ~= nil then
+		vim.schedule(tbl.extras_func)
+	end
+end
+
+-- init colours
+set_colours(scheme_default)
+
+local function contains(tbl, val)
+	for _, value in ipairs(tbl) do
+		if value == val then
+			return true
+		end
+	end
+	return false
+end
+
+local group = vim.api.nvim_create_augroup("filetype_colours", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", { -- perform only once on loading
+	nested = true, --allow ColorScheme event to also fire.
+	pattern = "*",
+	group = group,
+	callback = function()
+		if not enabled then
+			return
+		end
+
+		local ft = vim.bo.filetype
+
+		if vim.bo.buftype ~= "" then
+			return
+		end
+
+		if contains(buffers_text, ft) then
+			set_colours(scheme_text)
+			return
+		end
+
+		if contains(buffers_code, ft) then
+			set_colours(scheme_code)
+			return
+		end
+	end,
+})
+
 return {
-	-- Default/Code
-	{ "EdenEast/nightfox.nvim" },
-	-- { "vague-theme/vague.nvim", opts = {} },
-	--"catppuccin/nvim",
-	--"lifepillar/gruvbox8",
+	-- Default / code
+	{
+		"yorickpeterse/vim-paper",
+		--		enabled = false,
+	},
 
 	-- Writing
 	{ "savq/melange-nvim" },
 	--	{ "yorik1984/newpaper.nvim" },
 	--"rebelot/kanagawa.nvim",
 
-	{
-		"yorickpeterse/vim-paper",
-		--		enabled = false,
-		config = function()
-			local function set_colours(scheme)
-				local tbl = vim.tbl_deep_extend("force", scheme_default, scheme)
-
-				if tbl.colo ~= nil then
-					vim.cmd("colo " .. tbl.colo)
-				end
-				if tbl.bg ~= nil then
-					vim.cmd("set bg=" .. tbl.bg)
-				end
-				if tbl.extras_func ~= nil then
-					tbl.extras_func()
-				end
-			end
-
-			-- init colours
-			set_colours(scheme_default)
-
-			local function contains(tbl, val)
-				for _, value in ipairs(tbl) do
-					if value == val then
-						return true
-					end
-				end
-				return false
-			end
-
-			local group = vim.api.nvim_create_augroup("filetype_colours", { clear = true })
-			vim.api.nvim_create_autocmd("BufEnter", { -- perform only once on loading
-				nested = true, --allow ColorScheme event to also fire.
-				pattern = "*",
-				group = group,
-				callback = function()
-					if not enabled then
-						return
-					end
-
-					local ft = vim.bo.filetype
-
-					if vim.bo.buftype ~= "" then
-						return
-					end
-
-					if contains(buffers_text, ft) then
-						set_colours(scheme_text)
-						return
-					end
-
-					if contains(buffers_code, ft) then
-						set_colours(scheme_code)
-						return
-					end
-				end,
-			})
-		end,
-	},
+	{ "EdenEast/nightfox.nvim" },
+	-- { "vague-theme/vague.nvim", opts = {} },
+	--"catppuccin/nvim",
+	--"lifepillar/gruvbox8",
 }
